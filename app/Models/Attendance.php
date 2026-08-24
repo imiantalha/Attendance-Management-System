4a70f478b5acaac8027f9735262c55d5a1e1364a
Muhammad Talha <117847229+imiantalha@users.noreply.github.com>
2026-08-24 13:26:08 -0700

    refactor(attendance): cast attendance dates consistently

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -14,17 +14,20 @@ class Attendance extends Model
         'end_time',
         'attendance_date',
         'user_id',
-        'attendance_by'
+        'attendance_by',
     ];
 
-    public function user() 
+    protected $casts = [
+        'attendance_date' => 'date',
+    ];
+
+    public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
     }
 
-    public function attendedBy() 
+    public function attendedBy()
     {
         return $this->belongsTo(User::class, 'attendance_by');
     }
-
 }
