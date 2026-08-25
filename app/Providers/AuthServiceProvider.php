00023ea61d8b40e889de3bc6593fe9a3bb2f765c
Muhammad Talha <117847229+imiantalha@users.noreply.github.com>
2026-08-25 10:43:23 -0700

    feat(auth): register resource policies

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,13 +3,22 @@
 namespace App\Providers;
 
 use App\Models\Attendance;
+use App\Models\Product;
+use App\Models\User;
 use App\Policies\AttendancePolicy;
+use App\Policies\ProductPolicy;
+use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Spatie\Permission\Models\Role;
+use App\Policies\RolePolicy;
 
 class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [
         Attendance::class => AttendancePolicy::class,
+        Product::class => ProductPolicy::class,
+        Role::class => RolePolicy::class,
+        User::class => UserPolicy::class,
     ];
 
     public function boot(): void
